e54b1dd03561a2f2f3f5b121e18d82edcf0e9e3c
Anna Su <newstory0113@gmail.com>
2018-05-14 21:10:22 +0800

    feat(course): adjust file path for build

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ x('https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html', 'table > tr', [{
     no: 'td:nth-child(2)',
     courseName: 'td:nth-child(4)',
     teather: 'td:nth-child(5)',
-}]).write('./course.json');
+}]).write('./dist/course.json');
