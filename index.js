d692844aba2d10a60cb265986df413acbe9c0de8
Anna Su <newstory0113@gmail.com>
2018-05-14 20:59:21 +0800

    feat(course): scraper course data

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello Anna :)');
+const Xray = require('x-ray');
+const x = Xray();
+
+// https://www.npmjs.com/package/x-ray
+// https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html
+// 爬資料: 編號, 科目名稱, 任課教授
+
+x('https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html', 'table > tr', [{
+    no: 'td:nth-child(2)',
+    courseName: 'td:nth-child(4)',
+    teather: 'td:nth-child(5)',
+}]).write('./course.json');
