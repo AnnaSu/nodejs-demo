94bc37caa95db23491a45a389306fe0c9870701a
Anna Su <newstory0113@gmail.com>
2018-05-15 20:14:41 +0800

    feat(express): simple express demo

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const Xray = require('x-ray');
-const x = Xray();
+const Express = require('express');
 
-// https://www.npmjs.com/package/x-ray
-// https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html
-// 爬資料: 編號, 科目名稱, 任課教授
+const app = Express();
 
-x('https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html', 'table > tr', [{
-    no: 'td:nth-child(2)',
-    courseName: 'td:nth-child(4)',
-    teather: 'td:nth-child(5)',
-}]).write('./dist/course.json');
+app.get('/', function (req, res) {
+    console.log('Hello');
+    res.send('Hello World');
+});
+
+app.listen(3000, function () {
+    console.log('start listen http://localhost:3000');
+})
