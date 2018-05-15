631666eac8f08a79f43a07881f279182bcd2462c
Anna Su <newstory0113@gmail.com>
2018-05-15 21:20:52 +0800

    feat(helmet, serve-static): simple demo

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const Express = require('express');
+const helmet = require('helmet');
+const serveStatic = require('serve-static');
 
 const app = Express();
+app.use(helmet());
+// 當作靜態檔案輸出，如果沒有，再去找其他的 route
+// localhost:3000/avatar.jpg
+app.use(serveStatic('public'));
 
+// 一定要寫在所有 router 前面
+// 會先進到這裡，印出 middleware，接著 next() 找到下一個路徑
+app.use((req, res, next) => {
+    console.log('middleware');
+    next();
+})
+
+app.get('/home', function (req, res) { 
+    console.log('Hello');
+    res.send('Hello Home');
+});
+
+app.get('/user/:id', (req, res) => {
+    console.log('params:', req.params);
+    console.log('id', req.params.id);
+    res.send('user id: ' + req.params.id);
+});
+ 
 app.get('/', function (req, res) {
     console.log('Hello');
     res.send('Hello World');
