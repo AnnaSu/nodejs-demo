ec6788ad67e3a066b618d0390237708aa334c08a
Anna Su <newstory0113@gmail.com>
2018-05-15 21:22:38 +0800

    feat(listen port): modify listen port

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('start listen http://localhost:3000');
 })
